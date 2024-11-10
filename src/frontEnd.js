06a169e48166d549108087be3b0c9f00dfc8cb4a
Sand <sandboxasdf@gmail.com>
2024-11-10 17:42:33 +1100

    test

--- src/frontEnd.js
+++ src/frontEnd.js
@@ -81,7 +81,6 @@ export const FrontEnd = (function () {
         _quests.forEach((quest) => {
             CreateNewQuestHTML(quest);
         });
-
     }
 
     function CreateNewQuestHTML(quest) {
